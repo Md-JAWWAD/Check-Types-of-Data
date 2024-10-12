4b451ad80ef7882e87d082133a4bd6669ac547c7
Muhammad Jawwad <themdjawwad@gmail.com>
2024-10-12 15:46:37 +0500

    Update script.js
    Here we do a Recursion method.

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function checkTypesInArr(userArr) {
             booleansArr++;
         } else if (Array.isArray(userArr[i])) {
             nestedArr++;
+            checkTypesInArr(userArr[i])
         }
     }
 }
